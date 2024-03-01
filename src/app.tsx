907d57fc8163a9c88c5d99147518e47655a7cce2
Gaston <gastonmartinezcastro@gmail.com>
2024-03-01 00:31:37 -0300

    Instalacion webpack

---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import '../styles.css'
+
+const root = ReactDOM.createRoot(document.querySelector(".app"))
+root.render(<p>Hello World</p>)
